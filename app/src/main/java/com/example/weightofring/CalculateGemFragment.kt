38999242b5bf24c467f3890a50d83b468f307fff
Eugene <gladchuk2206@mail.ru>
2022-11-03 17:29:02 +0300

    перенес логику из активити в фрагменты

--- app/src/main/java/com/example/weightofring/CalculateGemFragment.kt
+++ app/src/main/java/com/example/weightofring/CalculateGemFragment.kt
@@ -5,12 +5,180 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import com.example.weightofring.databinding.FragmentCalculateGemBinding
+import kotlin.math.floor
 
 
 class CalculateGemFragment : Fragment() {
 
     lateinit var binding: FragmentCalculateGemBinding
+
+    lateinit var adapter: ArrayAdapter<GemParameters>
+    lateinit var adapterCut: ArrayAdapter<CutType>
+
+    var imageArrayDiamond: IntArray = intArrayOf(
+        R.drawable.rounddiamond,
+        R.drawable.princdiamond,
+        R.drawable.ovaldiamond,
+        R.drawable.emerdiamond,
+        R.drawable.bagdiamond,
+        R.drawable.marcdiamond
+    )
+    var imageArrayRubin: IntArray = intArrayOf(
+        R.drawable.roundrubin,
+        R.drawable.princrub,
+        R.drawable.ovalrubi,
+        R.drawable.emerrubi,
+        R.drawable.bagrubi,
+        R.drawable.marcrubi
+    )
+    var imageArrayCitrin: IntArray = intArrayOf(
+        R.drawable.roundcitrin,
+        R.drawable.princcitrin,
+        R.drawable.ovalcitrin,
+        R.drawable.emercitrin,
+        R.drawable.bagcitrin,
+        R.drawable.marccitrin
+    )
+    var imageArrayEmerald: IntArray = intArrayOf(
+        R.drawable.roundemerald,
+        R.drawable.princemer,
+        R.drawable.ovalemer,
+        R.drawable.emeremer,
+        R.drawable.bagemer,
+        R.drawable.marcemer
+    )
+    var imageArrayAmethyst: IntArray = intArrayOf(
+        R.drawable.roundamethyst,
+        R.drawable.princamet,
+        R.drawable.ovalamet,
+        R.drawable.emeramet,
+        R.drawable.bagamet,
+        R.drawable.marcamet
+    )
+    var imageArrayAquamarine: IntArray = intArrayOf(
+        R.drawable.roundaqua,
+        R.drawable.princaqua,
+        R.drawable.ovalaqua,
+        R.drawable.emeraqua,
+        R.drawable.bagaqua,
+        R.drawable.marcaqua
+    )
+
+
+    val gemParameters = mutableListOf(
+        GemParameters(
+            nameGem = "Diamond",
+            densityGem = "3.52",
+            nameEnum = GemParameters.NameGemEnum.DIAMOND
+        ),
+        GemParameters(
+            nameGem = "Rubin",
+            densityGem = "3.99",
+            nameEnum = GemParameters.NameGemEnum.RUBIN
+        ),
+        GemParameters(
+            nameGem = "Emerald",
+            densityGem = "2.71",
+            nameEnum = GemParameters.NameGemEnum.EMERALD
+        ),
+        GemParameters(
+            nameGem = "Сitrine",
+            densityGem = "2.65",
+            nameEnum = GemParameters.NameGemEnum.CITRINE
+        ),
+        GemParameters(
+            nameGem = "Amethyst",
+            densityGem = "2.65",
+            nameEnum = GemParameters.NameGemEnum.AMETHYST
+        ),
+        GemParameters(
+            nameGem = "Aquamarine",
+            densityGem = "2.69",
+            nameEnum = GemParameters.NameGemEnum.AQUAMARINE
+        )
+    )
+
+    val cutType = mutableListOf(
+        CutType(name = "Round", form = CutType.CutForm.ROUND, calculationCoefficient = "0.0018"),
+        CutType(name = "Princess", form = CutType.CutForm.PRINCESS, calculationCoefficient = "0.0023"),
+        CutType(name = "Oval", form = CutType.CutForm.OVAL, calculationCoefficient = "0.0018"),
+        CutType(name = "Emerald", form = CutType.CutForm.EMERALD, calculationCoefficient = "0.00245"),
+        CutType(name = "Baguette", form = CutType.CutForm.BAGUETTE, calculationCoefficient = "0.0029"),
+        CutType(name = "Marquis", form = CutType.CutForm.MARQUIS, calculationCoefficient = "0.0016")
+    )
+    var selectedCutParameters: CutType = cutType[0]
+
+    var selectedGemParameter: GemParameters = gemParameters[0]
+
+    private val cutTypeListener = object : AdapterView.OnItemSelectedListener {
+        override fun onItemSelected(p0: AdapterView<*>?, view: View?, position: Int, id: Long) {
+            val differentTypeCut = cutType[position]
+            selectedCutParameters = differentTypeCut
+
+            val positionCut = selectedCutParameters.form.position
+
+            when (selectedGemParameter.nameEnum) {
+                GemParameters.NameGemEnum.DIAMOND -> {
+                    binding.imageView.setImageResource(imageArrayDiamond[positionCut])
+                }
+                GemParameters.NameGemEnum.RUBIN -> {
+                    binding.imageView.setImageResource(imageArrayRubin[positionCut])
+                }
+                GemParameters.NameGemEnum.EMERALD -> {
+                    binding.imageView.setImageResource(imageArrayEmerald[positionCut])
+                }
+                GemParameters.NameGemEnum.CITRINE -> {
+                    binding.imageView.setImageResource(imageArrayCitrin[positionCut])
+                }
+                GemParameters.NameGemEnum.AMETHYST -> {
+                    binding.imageView.setImageResource(imageArrayAmethyst[positionCut])
+                }
+                GemParameters.NameGemEnum.AQUAMARINE -> {
+                    binding.imageView.setImageResource(imageArrayAquamarine[positionCut])
+                }
+            }
+        }
+
+        override fun onNothingSelected(position: AdapterView<*>?) {
+        }
+    }
+
+
+    private val gemParametersListener = object : AdapterView.OnItemSelectedListener {
+        override fun onItemSelected(p0: AdapterView<*>?, view: View?, positionMat: Int, id: Long) {
+            val differentDensity = gemParameters[positionMat]
+            selectedGemParameter = differentDensity
+
+            val positionCut = selectedCutParameters.form.position
+
+            when (selectedGemParameter.nameEnum) {
+                GemParameters.NameGemEnum.DIAMOND -> {
+                    binding.imageView.setImageResource(imageArrayDiamond[positionCut])
+                }
+                GemParameters.NameGemEnum.RUBIN -> {
+                    binding.imageView.setImageResource(imageArrayRubin[positionCut])
+                }
+                GemParameters.NameGemEnum.EMERALD -> {
+                    binding.imageView.setImageResource(imageArrayEmerald[positionCut])
+                }
+                GemParameters.NameGemEnum.CITRINE -> {
+                    binding.imageView.setImageResource(imageArrayCitrin[positionCut])
+                }
+                GemParameters.NameGemEnum.AMETHYST -> {
+                    binding.imageView.setImageResource(imageArrayAmethyst[positionCut])
+                }
+                GemParameters.NameGemEnum.AQUAMARINE -> {
+                    binding.imageView.setImageResource(imageArrayAquamarine[positionCut])
+                }
+            }
+        }
+
+        override fun onNothingSelected(position: AdapterView<*>?) {
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,6 +187,118 @@ class CalculateGemFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = FragmentCalculateGemBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        setupListParameters()
+        setupListParameters2()
+
+        binding.buttonResultGem.setOnClickListener {
+            if (binding.lengthEditText.text.isBlank()) {
+                binding.lengthEditText.error = ("empty")
+            } else if (binding.widthEditText.text.isBlank()) {
+                binding.widthEditText.error = ("empty")
+            } else if (binding.depthEditText.text.isBlank()) {
+                binding.depthEditText.error = ("empty")
+            } else {
+                gemWeightResult()
+            }
+
+        }
+
+    }
+
+    private fun setupListParameters() {
+        adapter = ArrayAdapter(
+            requireContext(),
+            android.R.layout.simple_list_item_1,
+            android.R.id.text1,
+            gemParameters
+        )
+        binding.densitySpinner.adapter = adapter
+
+        binding.densitySpinner.onItemSelectedListener = gemParametersListener
+
+    }
+
+    private fun setupListParameters2() {
+        adapterCut = ArrayAdapter(
+            requireContext(),
+            android.R.layout.simple_list_item_1,
+            android.R.id.text1,
+            cutType
+        )
+        binding.cutSpinner.adapter = adapterCut
+
+        binding.cutSpinner.onItemSelectedListener = cutTypeListener
+
+    }
+
+    private fun gemWeightResult() {
+
+        val lengthGem = binding.lengthEditText.text.toString().toDouble()
+        val widthGem = binding.widthEditText.text.toString().toDouble()
+        val depthGem = binding.depthEditText.text.toString().toDouble()
+        val densityGemCalculate = selectedGemParameter.densityGem.toDouble()
+        val typeCutCalculate = selectedCutParameters.calculationCoefficient.toDouble()
+
+        val sizeGem: Double
+        val sizePrincess = (lengthGem + widthGem) / 2
+
+        if (selectedCutParameters.name == "Oval") {
+            sizeGem = sizePrincess * sizePrincess * depthGem
+        } else {
+            sizeGem = lengthGem * widthGem * depthGem
+        }
+
+        val gemDensity = sizeGem * densityGemCalculate
+        val gemWeightResult = gemDensity * typeCutCalculate
+        val gemWeightResultFloor = floor(gemWeightResult * 1000.0) / 1000.0
+
+        val gemWeightGramms = gemWeightResult * 0.2
+        val gemWeightGrammsFloor = floor(gemWeightGramms * 1000.0) / 1000.0
+
+        gemWeightResultFloor.toString().also { binding.textViewResult.text = it }
+        gemWeightGrammsFloor.toString().also { binding.resultGemGrammTV.text = it }
+
+    }
+
+    class GemParameters(
+        val nameGem: String,
+        val densityGem: String,
+        val nameEnum: NameGemEnum
+    ) {
+        override fun toString(): String {
+            return nameGem
+        }
+
+        enum class NameGemEnum {
+            DIAMOND,
+            RUBIN,
+            EMERALD,
+            CITRINE,
+            AMETHYST,
+            AQUAMARINE
+        }
+
+    }
+
+    class CutType(
+        val name: String,
+        val form: CutForm,
+        val calculationCoefficient: String
+    ) {
+        override fun toString(): String {
+            return name
+        }
+        enum class CutForm(val position: Int) {
+            ROUND(0),
+            PRINCESS(1),
+            OVAL(2),
+            EMERALD(3),
+            BAGUETTE(4),
+            MARQUIS(5)
+        }
     }
 
     companion object {
--- app/src/main/java/com/example/weightofring/CalculateRingFragment.kt
+++ app/src/main/java/com/example/weightofring/CalculateRingFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.weightofring.databinding.FragmentCalculateRingBinding
+import kotlin.math.floor
 
 
 class CalculateRingFragment : Fragment() {
@@ -20,6 +21,52 @@ class CalculateRingFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = FragmentCalculateRingBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.buttonResult.setOnClickListener {
+            if (binding.editTextWidthRing.text.isBlank()) {
+                binding.editTextWidthRing.error = ("empty")
+            } else if (binding.editTextSizeRing.text.isBlank()) {
+                binding.editTextSizeRing.error = ("empty")
+            } else if (binding.editTextThicknessRing.text.isBlank()) {
+                binding.editTextThicknessRing.error = ("empty")
+            } else {
+                resultview()
+            }
+
+        }
+    }
+
+    private fun resultview(): Boolean {
+        var size = binding.editTextSizeRing.text.toString().toDouble()
+        var width = binding.editTextWidthRing.text.toString().toDouble()
+        var thickness = binding.editTextThicknessRing.text.toString().toDouble()
+
+        var areaSize = ((size * size)/4)*3.14
+        var insideDiameter = (thickness * 2) + size
+        var areaTotal = ((insideDiameter * insideDiameter)/4)*3.14
+        var areaRingSide = areaTotal - areaSize
+        var volumeRing = areaRingSide * width
+
+        var gold750 = 0.0154 * volumeRing
+        var gold750Floor = floor(gold750 * 100.0) /100.0
+        var gold585 = 0.0138 * volumeRing
+        var gold585Floor = floor(gold585 * 100.0) /100.0
+        var silver = 0.01036 * volumeRing
+        var silverFloor = floor(silver * 100.0) /100.0
+
+
+        if (binding.radioButtonGold750.isChecked) {
+            gold750Floor.toString().also { binding.textViewResult.text = it }
+        }
+        else if (binding.radioButtonGold585.isChecked) {
+            gold585Floor.toString().also { binding.textViewResult.text = it }
+        }
+        else if (binding.radioButtonSilver.isChecked) {
+            silverFloor.toString().also { binding.textViewResult.text = it }
+        }
+        return true
     }
 
     companion object {
