42341baf82979601133a2fdf36bf31a95c6a72e4
Eugene <gladchuk2206@mail.ru>
2023-03-13 00:38:03 +0300

    перенес листы из GemFragmentViewModel в отдельный класс

--- app/src/main/java/com/example/weightofring/ui/viewModels/CalculateGemViewModel.kt
+++ app/src/main/java/com/example/weightofring/ui/viewModels/CalculateGemViewModel.kt
@@ -1,91 +1,27 @@
 package com.example.weightofring.ui.viewModels
 
-
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.weightofring.domain.GemDrawablesStore.getGemDrawable
 import com.example.weightofring.domain.model.CutType
 import com.example.weightofring.domain.model.GemParameters
+import com.example.weightofring.domain.model.Lists
 import java.lang.Math.floor
 
 class CalculateGemViewModel : ViewModel() {
+
+    val lists = Lists()
 
     data class CutEditTextState(
         val text: String,
         val error: Boolean
     )
 
-    private val _allGemParameters = MutableLiveData<List<GemParameters>>(
-        listOf(
-            GemParameters(
-                nameGem = "Diamond",
-                densityGem = "3.52",
-                nameEnum = GemParameters.NameGemEnum.DIAMOND
-            ),
-            GemParameters(
-                nameGem = "Rubin",
-                densityGem = "3.99",
-                nameEnum = GemParameters.NameGemEnum.RUBIN
-            ),
-            GemParameters(
-                nameGem = "Emerald",
-                densityGem = "2.71",
-                nameEnum = GemParameters.NameGemEnum.EMERALD
-            ),
-            GemParameters(
-                nameGem = "Сitrine",
-                densityGem = "2.65",
-                nameEnum = GemParameters.NameGemEnum.CITRINE
-            ),
-            GemParameters(
-                nameGem = "Amethyst",
-                densityGem = "2.65",
-                nameEnum = GemParameters.NameGemEnum.AMETHYST
-            ),
-            GemParameters(
-                nameGem = "Aquamarine",
-                densityGem = "2.69",
-                nameEnum = GemParameters.NameGemEnum.AQUAMARINE
-            )
-        )
-    )
+    private val _allGemParameters = MutableLiveData(lists.listGemParameters)
     val allGemParameters: LiveData<List<GemParameters>> get() = _allGemParameters
 
-    private val _allCutParameters = MutableLiveData(
-        listOf(
-            CutType(
-                name = "Round",
-                form = CutType.CutForm.ROUND,
-                calculationCoefficient = "0.0018"
-            ),
-            CutType(
-                name = "Princess",
-                form = CutType.CutForm.PRINCESS,
-                calculationCoefficient = "0.0023"
-            ),
-            CutType(
-                name = "Oval",
-                form = CutType.CutForm.OVAL,
-                calculationCoefficient = "0.0018"
-            ),
-            CutType(
-                name = "Emerald",
-                form = CutType.CutForm.EMERALD,
-                calculationCoefficient = "0.00245"
-            ),
-            CutType(
-                name = "Baguette",
-                form = CutType.CutForm.BAGUETTE,
-                calculationCoefficient = "0.0029"
-            ),
-            CutType(
-                name = "Marquis",
-                form = CutType.CutForm.MARQUIS,
-                calculationCoefficient = "0.0016"
-            )
-        )
-    )
+    private val _allCutParameters = MutableLiveData(lists.listCutParameters)
     val allCutParameters: LiveData<List<CutType>> get() = _allCutParameters
 
     private val _gemImage = MutableLiveData<Int>()
